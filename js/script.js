3bd330aa4e6f1804211e27c1f9f74711bb6a4607
PatelShiv7 <61666967+PatelShiv7@users.noreply.github.com>
2021-05-23 18:42:57 +1200

    Categories page - Attempting to fix filter
    Still having the same problem with the filtering system. Will try to fix it tomorrow

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ const toggleBtn1 = document.querySelector("#toggleBtn1");
 const divList1 = document.querySelector("#science");
 
 toggleBtn1.addEventListener('click', () => {
-    if(divList1.style.visibility === 'hidden') {
+    if(divList1.style.display === 'none') {
         toggleBtn1.value = "HIDE SCIENCE";
-        divList1.style.visibility = 'visible';
+        divList1.style.display = 'inline-block';
     } else {
         toggleBtn1.value = "SHOW SCIENCE";
-        divList1.style.visibility = 'hidden';
+        divList1.style.display = 'none';
     }
 })
 
@@ -16,12 +16,12 @@ const divList2 = document.querySelectorAll(".architecture");
 
 toggleBtn2.addEventListener('click', () => {
     for(var i = 0; i < divList2.length; i++) {
-        if(divList2[i].style.visibility === 'hidden') {
+        if(divList2[i].style.display === 'none') {
             toggleBtn2.value = "HIDE ARCHITECTURE"
-            divList2[i].style.visibility = 'visible';
+            divList2[i].style.display = 'inline-block';
         } else {
             toggleBtn2.value = "SHOW ARCHITECTURE"
-            divList2[i].style.visibility = 'hidden';
+            divList2[i].style.display = 'none';
         }
     }
 
@@ -32,14 +32,27 @@ const divList3 = document.querySelectorAll(".design");
 
 toggleBtn3.addEventListener('click', () => {
     for(var i = 0; i < divList3.length; i++) {
-        if(divList3[i].style.visibility === 'hidden') {
+        if(divList3[i].style.display === 'none') {
             toggleBtn3.value = "HIDE DESIGN"
-            divList3[i].style.visibility = 'visible';
+            divList3[i].style.display = 'inline-block';
         } else {
             toggleBtn3.value = "SHOW DESIGN"
-            divList3[i].style.visibility = 'hidden';
+            divList3[i].style.display = 'none';
+
         }
     }
+
+})
+
+toggleBtn3.addEventListener("click", function(){
+    let elem = document.getElementsByClassName("box-grid");
+    elem.style.gridTemplateAreas = 
+        '"aside1 heading heading heading aside2" "aside1 section1 section1 section1 aside2" "aside1 section3 section3 section3 aside2" "aside1 section4 section4 section4 aside2" "aside1 section5 section5 section5 aside2"';
+        
+        
+        
+        
+        
 
 })
 
