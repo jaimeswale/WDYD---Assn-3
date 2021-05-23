cde52d9eb9c922774f9e6a58c7504eb78794e3a3
PatelShiv7 <61666967+PatelShiv7@users.noreply.github.com>
2021-05-23 18:17:56 +1200

    Updating categories page
    Grid system is fixed. When the user filters out a category, the elements inside are hidden. Looking for a way so that the other sections move up on the screen. - Currently working with Jaime for this problem

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ const toggleBtn1 = document.querySelector("#toggleBtn1");
 const divList1 = document.querySelector("#science");
 
 toggleBtn1.addEventListener('click', () => {
-    if(divList1.style.display === 'none') {
+    if(divList1.style.visibility === 'hidden') {
         toggleBtn1.value = "HIDE SCIENCE";
-        divList1.style.display = 'block';
+        divList1.style.visibility = 'visible';
     } else {
         toggleBtn1.value = "SHOW SCIENCE";
-        divList1.style.display = 'none';
+        divList1.style.visibility = 'hidden';
     }
 })
 
@@ -16,12 +16,12 @@ const divList2 = document.querySelectorAll(".architecture");
 
 toggleBtn2.addEventListener('click', () => {
     for(var i = 0; i < divList2.length; i++) {
-        if(divList2[i].style.display === 'none') {
+        if(divList2[i].style.visibility === 'hidden') {
             toggleBtn2.value = "HIDE ARCHITECTURE"
-            divList2[i].style.display = 'block';
+            divList2[i].style.visibility = 'visible';
         } else {
             toggleBtn2.value = "SHOW ARCHITECTURE"
-            divList2[i].style.display = 'none';
+            divList2[i].style.visibility = 'hidden';
         }
     }
 
@@ -32,12 +32,12 @@ const divList3 = document.querySelectorAll(".design");
 
 toggleBtn3.addEventListener('click', () => {
     for(var i = 0; i < divList3.length; i++) {
-        if(divList3[i].style.display === 'none') {
+        if(divList3[i].style.visibility === 'hidden') {
             toggleBtn3.value = "HIDE DESIGN"
-            divList3[i].style.display = 'block';
+            divList3[i].style.visibility = 'visible';
         } else {
             toggleBtn3.value = "SHOW DESIGN"
-            divList3[i].style.display = 'none';
+            divList3[i].style.visibility = 'hidden';
         }
     }
 
